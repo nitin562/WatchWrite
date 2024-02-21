b3f2ba3372562fdd272e9da52074d5ffc0ff7d7f
nitin562 <nitindbas8800@gmail.com>
2024-02-21 21:17:20 +0530

    Added login endpoint

--- Backend/API/user.api.js
+++ Backend/API/user.api.js
@@ -1,15 +1,16 @@
-const {check} = require("express-validator")
+const {check, param} = require("express-validator")
 const express=require("express")
 const { Register, Login } = require("../controllers/user.controller")
 const Router=express.Router()
 //success=-1 error from client
 //success=0 error from server
 //success=1 correct
-//success=-2 already registered
+//success=-2 already registered in sign or not registered in Login
+//success=-3 password is wrong in login
 //1.Sign
 Router.post("/sign",[check("name","Name must be of atleast 3 characters").isLength({min:3}),check("email","Email is not valid").isEmail(),check("password","Password must be of atleast 6 characters").isLength({min:6})],Register)
 
 //2.Login
-Router.get("/login",[check("email","Email is invalid").isEmail(),check("password","Password must be of atleast 6 characters").isLength({min:6})],Login)
+Router.get("/login/:email/:password",[param("email","Email is invalid").isEmail(),param("password","Password must be of atleast 6 characters").isLength({min:6})],Login)
 
 module.exports=Router
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,39 +1,76 @@
-const {validationResult}=require("express-validator");
+const { validationResult } = require("express-validator");
 const User = require("../models/user.models");
-const { hashPassword } = require("../Utils/Helper/Password");
+const { hashPassword, verifyPassword } = require("../Utils/Helper/Password");
 const { createToken } = require("../Utils/Helper/Token");
 const Register = async (req, res) => {
   try {
-    const error=validationResult(req);
-    if(!error.isEmpty()){//client side error
-        return res.status(400).json({success:-1,error:error.mapped()})
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      //client side error
+      return res.status(400).json({ success: -1, error: error.mapped() });
     }
     //checking email exist in db already or not
-    const {email}=req.body
-    const client=await User.findOne({email})
-    if(client){
-        //already exist
-        return res.status(200).json({success:-2,msg:"Already registered"})
+    const { email } = req.body;
+    const client = await User.findOne({ email });
+    if (client) {
+      //already exist
+      return res.status(200).json({ success: -2, msg: "Already registered" });
     }
-    //means you need to register it 
+    //means you need to register it
     //Hash the password
-    const {password,name}=req.body
-    const HashedPassword=await hashPassword(password)
-    const Record=await User.create({userName:name,password:HashedPassword,email})
-    const token=createToken(Record._id)
-    res.cookie(token,{path:"/",domain:`${process.env.base}/api`,httpOnly:true,secure:true,sameSite:true})
-    return res.status(200).json({success:1,token})
-    
+    const { password, name } = req.body;
+    const HashedPassword = await hashPassword(password);
+    const Record = await User.create({
+      userName: name,
+      password: HashedPassword,
+      email,
+    });
+    const token = createToken(Record._id);
+    res.cookie(token, {
+      path: "/",
+      domain: `${process.env.base}/api`,
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+    });
+    return res.status(200).json({ success: 1, token });
   } catch (error) {
-    console.log(error)
-    return res.status(500).status({success:0})
+    console.log(error);
+    return res.status(500).status({ success: 0 });
   }
 };
-const Login = async(req,res)=>{
+const Login = async (req, res) => {
   try {
-    
+
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ success: -1, error: error.mapped() });
+    }
+    //finding email
+    const { email } = req.params;
+    const client = await User.findOne({ email });
+    if (!client) {
+      return res.status(400).json({ success: -2, error: "Not Registered" });
+    }
+    //account found
+    const { password } = req.params;
+    const checkPassword = await verifyPassword(password, client.password);
+    if (!checkPassword) {
+      //means password is wrong
+      return res.status(400).json({ success: -3, error: "Password is wrong" });
+    }
+    //now password is matched so create token
+    const token = createToken(client._id);
+    res.cookie(token, {
+      httpOnly: true,
+      sameSite: true,
+      secure: true,
+      domain: process.env.base,
+    });
+    return res.status(200).json({ success: 1, token });
   } catch (error) {
-    
+    console.log(error);
+    return res.status(500).json({ sucess: 0, error });
   }
-}
-module.exports = { Register,Login };
+};
+module.exports = { Register, Login };
